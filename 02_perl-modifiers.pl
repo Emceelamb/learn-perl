121adf73329c770c856918e02bb1031cfa31b3d2
emceelamb <mlam@berkeley.edu>
2019-05-14 17:46:35 -0400

    perl arrays

---
+++ 02_perl-modifiers.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl -w
+use strict;  # Good practice
+             # Allows for modifiers
+
+my $var=5;
+
+if(1){
+    my $var_2=$var;
+    print "var_2 is $var_2";
+}
+print "var is $var";
+
